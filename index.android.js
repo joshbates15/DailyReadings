dea64c61f862ebf9aefcf680f1667b5d2b8e222e
joshbates15 <joshbates15@gmail.com>
2017-04-03 17:03:57 -0400

    Initial Sync

---
+++ index.android.js
@@ -0,0 +1,75 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Icon } from 'react-native-vector-icons/FontAwesome'
+
+export default class DailyReadings extends Component {
+  static navigationOptions = {
+    title: 'Readings For...',
+    header: {
+      right: <Button title="Settings" />,
+      left: <Button title="Menu" />,
+    },
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload,{'\n'}
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const AppStack = StackNavigator({
+  Readings: {
+    screen: ReadingsScene,
+  },
+  Calendar: { 
+    screen: CalendarScene,
+  },
+  Settings: {
+    screen: SettingsScene,
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('DailyReadings', () => DailyReadings);
